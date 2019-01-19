9438abb8a35edce3ecd632570ed53930a49f28a4
LB <luisbazan417@gmail.com>
2019-01-19 11:15:14 -0600

    CREATE Assets and Menu
    - Create Challenge assets added to project
    - Menu Scene created and programmed its functionality
    - Text Mesh Pro and Unity Standard Assets Added

---
+++ Assets/_Scripts/MenuUIController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUIController : MonoBehaviour
+{
+    public void OnStartClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
